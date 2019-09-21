14e109a9ae6c724e22b0aecc6401c7d9064340a2
Riza Fahmi <rizafahmi@gmail.com>
2019-09-21 10:08:37 +0700

    Initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<style>
+  main {
+    background-color: #262E36;
+  }
+</style>
+
+<main>
+  <slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<style>
+
+  h1 {
+    text-align: center;
+    margin: 0 auto;
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Ekosistem JavaScript di Indonesia</title>
+</svelte:head>
+
+<h1>Ekosistem JavaScript di Indonesia</h1>
+
