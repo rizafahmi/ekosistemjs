623db67707e8451994aee5d567e7afed54ccd66a
Riza Fahmi <rizafahmi@gmail.com>
2020-03-26 13:58:24 +0700

    Resize and use webp image format

---
+++ src/components/Image.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let name;
+  export let title;
+</script>
+<style>
+  picture {
+    display: flex;
+    justify-content: center;
+  }
+</style>
+<h3>{title}</h3>
+<a href="./img/{name}.png" target="_blank">
+  <picture>
+    <source type="image/webp" srcset="./img/{name}.webp" />
+    <source type="image/png" srcset="./img/{name}.png" />
+    <img src="img/{name}.png" alt="{name}" />
+  </picture>
+</a>
