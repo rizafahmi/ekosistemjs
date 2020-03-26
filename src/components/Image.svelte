1a981aaf6bb69f7380049c9b03086e95e5ee5fd6
Riza Fahmi <rizafahmi@gmail.com>
2020-03-26 17:07:10 +0700

    Add rel and meta tag

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -9,7 +9,7 @@
   }
 </style>
 <h3>{title}</h3>
-<a href="./img/{name}.png" target="_blank">
+<a rel="noopener" href="./img/{name}.png" target="_blank">
   <picture>
     <source type="image/webp" srcset="./img/{name}.webp" loading="lazy" />
     <source type="image/png" srcset="./img/{name}.png" loading="lazy" />
